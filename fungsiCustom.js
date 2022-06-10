b2c4b1fac0f22daf8a424648f5bbfbea097d1739
ramadhangandidi <thinkpad1st@gmail.com>
2022-06-10 15:21:58 +0700

    Percobaan pertama

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,38 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  arrString = [];
+
+  fs.readFile(file1, { encoding: "utf8" }, (err, data1) => {
+    if (err) {
+      return err;
+    } else {
+      let temp = JSON.parse(data1).message.split(" ");
+      arrString.push(temp[1]);
+    }
+
+    fs.readFile(file2, { encoding: "utf8" }, (err, data2) => {
+      if (err) {
+        return err;
+      } else {
+        let temp = JSON.parse(data2)[0].message.split(" ");
+        arrString.push(temp[1]);
+      }
+
+      fs.readFile(file3, { encoding: "utf8" }, (err, data3) => {
+        if (err) {
+          return err;
+        } else {
+          let temp = JSON.parse(data3)[0].data.message.split(" ");
+          arrString.push(temp[1]);
+        }
+      });
+    });
+  });
+
+  fnCallback(null, arrString);
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
